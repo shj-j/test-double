f76efd4655866119ee733aa1ea71e989a6c3ecf1
shj-j <shang.jie@outlook.com>
2020-08-04 23:33:43 +0800

    [sj]: implement GradeServiceTest

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,37 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+    @Mock
+    private GradeSystem gradeSystem;
+
+    @InjectMocks
+    private GradeService mockGradeService;
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        long studentID = 1;
+
+        when(gradeSystem.gradesFor(studentID)).thenReturn(Arrays.asList(100.0, 100.0, 100.0));
+
+        Assertions.assertEquals(100.0, mockGradeService.calculateAverageGrades(studentID));
+        verify(gradeSystem).gradesFor(studentID);
     }
 }
